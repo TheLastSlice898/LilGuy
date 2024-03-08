2eeb6a5c237fbbc55baa44a8ad7fb88144556b17
Bella <isabellajsneddon@outlook.com>
2024-03-08 14:23:52 +1100

    Text now spits out
    I AM LITERALLY SO SMART HOLY SHIT, I MADE THIS

---
+++ Assets/Scripts/BottomUIController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
+
+public class BottomUIController : MonoBehaviour
+{
+    [SerializeField]
+    public TextMeshProUGUI storyText; 
+    public TextMeshProUGUI characterNameText;
+
+    private int sentenceIndex = -1; 
+    public Scene currentScene;
+    private State state = State.Completed;
+    public float waitBetweenChar;
+
+    private enum State
+    {
+        Playing, Completed
+    }
+    void Start()
+    {
+        StartCoroutine(TypeText(currentScene.sentences[++sentenceIndex].text)); 
+    }
+
+    
+    private IEnumerator TypeText(string text)
+    {
+        storyText.text = "";    
+        state = State.Playing;
+        int wordIndex = 0;
+
+        while (state != State.Completed) 
+        {
+
+            storyText.text += text[wordIndex];
+            yield return new WaitForSeconds(0.01f* waitBetweenChar); 
+            if(++ wordIndex == text.Length)
+            {
+                state = State.Completed;
+                break;
+            }
+
+        }
+    } 
+    
+}
