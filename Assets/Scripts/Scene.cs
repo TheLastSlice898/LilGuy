f18a27c8f2125755022ac8ad0e3e1ff3ea71f194
Bella <isabellajsneddon@outlook.com>
2024-03-08 12:52:40 +1100

    Hello

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScene", menuName = "Data/Scene")]
+[System.Serializable]
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
