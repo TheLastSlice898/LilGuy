64eb6991c7bd29aa924a966f2100351e1148c278
citrinemoons <129808315+citrinemoons@users.noreply.github.com>
2024-03-27 15:39:59 +1100

    goodbye bella scene 1

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -22,25 +22,7 @@ public class DialogueManager : MonoBehaviour
 
     void Update()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         if(Input.GetKeyDown("space") && !GameManager.instance._pauseMenu)   //bella is a wirnkle brain
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        if(Input.GetKeyDown("space"))
-=======
-        if(Input.GetKeyDown("space") && !GameManager.instance._pauseMenu)   //bella is a wirnkle brain
->>>>>>> Stashed changes
-=======
-        if(Input.GetKeyDown("space") && !GameManager.instance._pauseMenu)   //bella is a wirnkle brain
->>>>>>> Stashed changes
->>>>>>> Bella
-=======
-
-        if(Input.GetKeyDown("space") && !GameManager.instance._pauseMenu)   //bella is a wirnkle brain
-
->>>>>>> Bella
          {
             if(textDialogue.text == lines[index]) 
             { 
